d1399c8e4de9890f0e4036df197a0d7ffd0c9afd
mombewa <ombewamaurice79@gmail.com>
2025-02-18 12:57:25 +0300

    Fix getPassword function to correctly retrieve user password

--- src/auth.c
+++ src/auth.c
@@ -3,54 +3,28 @@
 
 char *USERS = "./data/users.txt";
 
-void loginMenu(char a[50], char pass[50])
-{
-    struct termios oflags, nflags;
 
-    system("clear");
-    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login:");
-    scanf("%s", a);
-
-    // disabling echo
-    tcgetattr(fileno(stdin), &oflags);
-    nflags = oflags;
-    nflags.c_lflag &= ~ECHO;
-    nflags.c_lflag |= ECHONL;
-
-    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
-    {
-        perror("tcsetattr");
-        return exit(1);
-    }
-    printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
-    scanf("%s", pass);
-
-    // restore terminal
-    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
-    {
-        perror("tcsetattr");
-        return exit(1);
-    }
-};
 
 const char *getPassword(struct User u)
 {
     FILE *fp;
     struct User userChecker;
+    static char passwordBuffer[50];
 
     if ((fp = fopen("./data/users.txt", "r")) == NULL)
     {
-        printf("Error! opening file");
+        printf("Error! opening file\n");
         exit(1);
     }
 
-    while (fscanf(fp, "%s %s", userChecker.name, userChecker.password) != EOF)
+    while (fscanf(fp, "%d %s %s", &userChecker.id, userChecker.name, userChecker.password) == 3)
     {
         if (strcmp(userChecker.name, u.name) == 0)
         {
             fclose(fp);
-            char *buff = userChecker.password;
-            return buff;
+            strcpy(passwordBuffer, userChecker.password);
+            // char *buff = userChecker.password;
+            return passwordBuffer;
         }
     }
 
