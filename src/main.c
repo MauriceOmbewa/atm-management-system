806d5e9cb81b7d3c2c3cae6e0e219de71d3141c2
mombewa <ombewamaurice79@gmail.com>
2025-02-18 12:53:11 +0300

    Fix function flow to ensure correct user registration handling

--- src/main.c
+++ src/main.c
@@ -103,7 +103,6 @@ void registerUser(struct User *u)
 
 void initMenu(struct User *u)
 {
-    int r = 0;
     int option;
     system("clear");
     printf("\n\n\t\t======= ATM =======\n");
@@ -111,38 +110,41 @@ void initMenu(struct User *u)
     printf("\n\t\t[1]- login\n");
     printf("\n\t\t[2]- register\n");
     printf("\n\t\t[3]- exit\n");
-    while (!r)
+
+    scanf("%d", &option);
+    switch (option)
     {
-        scanf("%d", &option);
-        switch (option)
+    case 1:
+        loginMenu(u->name, u->password);
+        const char *savedPassword = getPassword(*u);
+        if (savedPassword == NULL)
         {
-        case 1:
-            loginMenu(u->name, u->password);
-            if (strcmp(u->password, getPassword(*u)) == 0)
-            {
-                printf("\n\nPassword Match!");
-            }
-            else
-            {
-                printf("\nWrong password!! or User Name\n");
-                exit(1);
-            }
-            r = 1;
-            break;
-        case 2:
-            // student TODO : add your **Registration** function
-            // here
-            registerUser(u);
-            r = 1;
-            break;
-        case 3:
+            printf("\nWrong password!! or User Name\n");
             exit(1);
-            break;
-        default:
-            printf("Insert a valid operation!\n");
         }
+
+        if (strcmp(u->password, savedPassword) == 0)
+        {
+            printf("\n\n✔ Password Match! Login successful!\n");
+        }
+        else
+        {
+            printf("\nWrong password!! or User Name\n");
+            exit(1);
+        }
+        return;
+
+    case 2:
+        registerUser(u);
+        return;
+
+    case 3:
+        exit(0);
+
+    default:
+        printf("Insert a valid operation!\n");
     }
-};
+}
 
 int main()
 {
